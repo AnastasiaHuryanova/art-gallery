ade4c52f34d5d49c67388ff940161147f7cf1fbd
Anastasia Huryanova <anastasia.huryanova@aesys.tech>
2023-03-28 12:53:04 +0200

    adds search by artist onClick

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,24 +1,43 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import PaintingList from "../Components/PaintingList";
 import { getAllArtworks, searchArtworksByArtist } from "../axios/artworks";
 
 const Gallery = () => {
   const [paintings, setPaintings] = useState([]);
+  const [artist, setArtist] = useState("monet");
 
   useEffect(() => {
     const loadPaintings = async () => {
       const config = { limit: 10 };
-      const fetchedArtworks = await searchArtworksByArtist(config, 'monet');
+      const fetchedArtworks = await getAllArtworks(config);
       setPaintings(fetchedArtworks);
     };
     loadPaintings();
-  }, []);
+  }, [])
+  
+
+  const loadPaintingsByArtist = async () => {
+    const config = { limit: 10 };
+    const fetchedArtworks = await searchArtworksByArtist(config, artist);
+    setPaintings(fetchedArtworks);
+  };
+
+  const handleChange = (e) => {
+    setArtist(e.target.value);
+  };
+
+  const handleClick = (e) => {
+    loadPaintingsByArtist();
+    e.preventDefault();
+    setArtist("");
+  };
 
   return (
     <div>
       <h1>gallery</h1>
-      <button>search</button>
+      <input type="text" onChange={handleChange} value={artist} />
+      {console.log(artist)}
+      <button onClick={handleClick}>search</button>
       <div>
         <PaintingList paintings={paintings} />
       </div>
